718bc6fac75457ff03cfe257fb08fe129ceda057
Crotear1 <138115674+Crotear1@users.noreply.github.com>
2024-07-02 17:53:09 +0200

    First commit

---
+++ config/session.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION["loggedIn"])) {
+    $_SESSION["loggedIn"] = 'notLoggedIn';
+}
+
+if (!isset($_SESSION["shoppingCart"])) {
+    $_SESSION["shoppingCart"] = [];
+}
+
+if (!isset($_SESSION["isAdmin"])) {
+    $_SESSION["shoppingCart"] = 'notAdmin';
+}
---
+++ register.php
@@ -0,0 +1,5 @@
+<?php
+include 'config/dbConncetion.php';
+include 'components/navigation.php';
+
+?>
